36e84ddbe42c4fcfe11dd61c0397e4f5456175ca
Aziell9 <aprisius.91@gmail.com>
2025-03-19 12:12:03 +0700

    first commit

---
+++ resources/views/dashboard/user.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Dashboard</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <h1>Welcome to User Dashboard</h1>
+    <p>Ini adalah halaman dashboard pengguna.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\DashboardUserController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard/user', [DashboardUserController::class, 'index']);
